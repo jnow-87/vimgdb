df6ad10288faca665228d1d90a532b8f4fecffff
jnow-87 <jan.nowotsch@gmail.com>
2015-01-10 17:26:38 +0100

    [Makefile]
    	adapt gperf rules to generate c++ code{
    
    		modified:   scripts/Makefile.build
    
    	}
    
    	add script to generate c++ header based on gperf output
    	add respective call to gperf makefile command{
    
    		modified:   scripts/Makefile.inc
    		new file:   scripts/gperf_header.sh
    
    	}

---
+++ scripts/gperf_header.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+#	generate header file based on gerf c++ output
+#		header will contain class definition extracted
+#		from c++ source
+#
+
+cfile=$1	# c++ source file
+header=$2	# output file name
+
+
+suffix=${cfile##*.}
+
+if [[ "${suffix}" != "cc" && "${suffix}" != "cpp" ]];then
+	echo -e $0: input file \"${cfile}\" is no C++ source file
+	exit 1
+fi
+
+# generate macro based on header file
+macro=$(basename ${header} | tr a-z. A-Z_)
+
+
+# print header
+echo -e "#ifndef "${macro} > ${header}
+echo -e "#define "${macro}"\n\n" >> ${header}
+
+# extract class
+sed -e '/class/,/}/!d' ${cfile} >> ${header}
+
+# print footer
+echo -e "\n\n#endif // "${macro} >> ${header}
+
+exit 0
