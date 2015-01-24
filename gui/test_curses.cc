1f537dcdfcd2a08840bcd20742b01d942294c45e
jnow-87 <jan.nowotsch@gmail.com>
2015-01-24 16:04:04 +0100

    [gui]
    	extend ncurses test to handle user input

--- gui/test_curses.cc
+++ gui/test_curses.cc
@@ -1,20 +1,57 @@
 #include <gui/curses.h>
+#include <common/tty.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define PROMPT	"~$ "
 
 
 int main(int argc, char** argv){
-	gui* g;
+	char c, line[255];
+	unsigned int i;
+	gui* ui;
+	tty std_term;
 
 
-	g = new curses();
-	g->init();
+	ui = new curses();
+	ui->init();
 
-	g->win_log("press 'q' to quit");
-	g->win_cmd("cmd example");
+	ui->win_log("enter \"quit\" to exit\n");
+	ui->win_cmd("insert commands here\n" PROMPT);
 
-	while(getchar() != 'q');
+	i = 0;
+	while(1){
+		std_term.read(&c, 1);
 
-	g->destroy();
-	delete (curses*)g;
+		switch(c){
+		case '\n':
+		case '\r':
+			line[i] = 0;
+			i = 0;
+
+			ui->win_log("exec: \"%s\"\n", line);
+			ui->win_cmd("\n" PROMPT);
+
+			if(strcmp(line, "quit") == 0 || strcmp(line, "q") == 0){
+				ui->win_log("exit\n");
+				sleep(1);
+				goto end;
+			}
+
+			break;
+		
+		default:
+			ui->win_cmd("%c", c);
+			ui->win_gdb_log("%c %d\n", c, (int)c);
+			line[i++] = c;
+			break;
+		};
+	}
+
+end:
+	ui->destroy();
+	delete (curses*)ui;
 
 	return 0;
 }
