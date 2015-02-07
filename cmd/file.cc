4f3261d67dc87e2eec50c6c223b89eb59c1a8974
jnow-87 <jan.nowotsch@gmail.com>
2015-02-07 08:17:58 +0100

    [cmd: file - user output]
    	- replace 'WARN' output for invalid arguments by 'USER'
    	- rephrase messages

--- cmd/file.cc
+++ cmd/file.cc
@@ -11,13 +11,9 @@ int cmd_file_exec(gdb_if* gdb, int argc, char** argv){
 	const struct subcmd_t* scmd;
 
 
-	if(argc < 2){
-		WARN("too few arguments to command \"%s\"\n", argv[0]);
-		return -1;
-	}
-
-	if(argc > 3){
-		WARN("too many arguments to command \"%s\", at most 2 expected\n", argv[0]);
+	if(argc < 2 || argc > 3){
+		USER("invalid number of arguments to command \"%s\"\n", argv[0]);
+		cmd_file_help(argv[0]);
 		return -1;
 	}
 
@@ -26,7 +22,7 @@ int cmd_file_exec(gdb_if* gdb, int argc, char** argv){
 	for(i=1; i<argc-1; i++){
 		scmd = subcmd::lookup(argv[i], strlen(argv[i]));
 		if(scmd == 0){
-			WARN("invalid sub command \"%s\" to \"%s\"\n", argv[i], argv[0]);
+			USER("invalid sub command \"%s\" to \"%s\"\n", argv[i], argv[0]);
 			return -1;
 		}
 
@@ -52,7 +48,7 @@ int cmd_file_exec(gdb_if* gdb, int argc, char** argv){
 int cmd_file_resp(result_class_t rclass, result_t* result, char* cmdline){
 	switch(rclass){
 	case RC_DONE:
-		USER("exec of command \"%s\" done\n", cmdline);
+		USER("done: exec \"%s\"\n", cmdline);
 		break;
 
 	case RC_ERROR:
