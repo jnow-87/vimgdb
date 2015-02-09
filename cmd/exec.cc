f6de2527860577f1f69b0faac938774e52c98c86
jnow-87 <jan.nowotsch@gmail.com>
2015-02-09 10:48:46 +0100

    [cmd: exec]
    	implement cmd_exec_help() and cmd_exec_resp()

--- cmd/exec.cc
+++ cmd/exec.cc
@@ -68,10 +68,71 @@ int cmd_exec_exec(gdb_if* gdb, int argc, char** argv){
 }
 
 int cmd_exec_resp(result_class_t rclass, result_t* result, char* cmdline, void* data){
-	TODO("not yet implemented\n");
+	switch(rclass){
+	case RC_DONE:
+	case RC_RUNNING:
+		USER("done: exec \"%s\"\n", cmdline);
+		break;
+
+	case RC_ERROR:
+		USER("gdb reported error for command \"%s\"\n\t%s\n", cmdline, result->value->value);
+		break;
+
+	default:
+		WARN("unhandled result class %d\n", rclass);
+		break;
+	};
+
 	return 0;
 }
 
 void cmd_exec_help(int argc, char** argv){
-	TODO("not yet implemented\n");
+	unsigned int i;
+	const struct subcmd_t* scmd;
+
+
+	if(argc == 1){
+		USER("usage: %s [sub-command] <args>...\n", argv[0]);
+		USER("   sub-commands:\n");
+		USER("      run              (re)start inferior execution\n");
+		USER("      continue         continue execution\n");
+		USER("      next             execute next line\n");
+		USER("      step             execute next line, enter functions\n");
+		USER("      return           execute until end of current function\n");
+		USER("      break            interrupt execution\n");
+		USER("      jump <location>  set PC to <location>\n");
+		USER("      goto <location>  execute until <location>\n");
+		USER("\n");
+	}
+	else{
+		for(i=1; i<argc; i++){
+			scmd = subcmd::lookup(argv[i], strlen(argv[i]));
+
+			if(scmd == 0){
+				USER("invalid sub-command \"%s\" to command \"%s\"\n", argv[i], argv[0]);
+				continue;
+			}
+
+			switch(scmd->id){
+			case JUMP:
+			case GOTO:
+				USER("usage %s %s <location>\n", argv[0], argv[i]);
+				USER("   <location> can be any of the following\n");
+				USER("      - line number in the current source file\n");
+				USER("      - [+-]offset\n");
+				USER("      - filename:line number\n");
+				USER("      - filename:function\n");
+				USER("      - function\n");
+				USER("      - function:label\n");
+				USER("      - label\n");
+				USER("      - *address\n");
+				USER("\n");
+				break;
+
+			default:
+				USER("no further help available for sub-command \"%s\"\n", argv[i]);
+			};
+		}
+	}
+
 }
