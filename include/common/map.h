0896f3fdf0cfa1499b109daf82aea904d002f94f
jnow-87 <jan.nowotsch@gmail.com>
2015-05-02 08:48:56 +0200

    [common: map macros]
    	add MAP_ERASE(), MAP_ERASE_SAFE()

--- include/common/map.h
+++ include/common/map.h
@@ -2,18 +2,32 @@
 #define COMMON_MAP_H
 
 
-#define MAP_LOOKUP(map, val) ({ \
+#define MAP_LOOKUP(map, val)({ \
 	auto it = (map).find(val); \
 	it == (map).end() ? 0 : it->second; \
 })
 
-#define MAP_LOOKUP_SAFE(map, val, mtx) ({ \
+#define MAP_LOOKUP_SAFE(map, val, mtx)({ \
 	pthread_mutex_lock(&(mtx)); \
 	auto it = (map).find(val); \
 	pthread_mutex_unlock(&(mtx)); \
 	\
 	it == (map).end() ? 0 : it->second; \
 })
+
+#define MAP_ERASE(map, val){ \
+	auto it = (map).find(val); \
+	if(it != (map).end()) \
+		(map).erase(it); \
+}
+
+#define MAP_ERASE_SAFE(map, val, mtx){ \
+	pthread_mutex_lock(&mtx); \
+	auto it = (map).find(val); \
+	if(it != (map).end()) \
+		(map).erase(it); \
+	pthread_mutex_unlock(&mtx); \
+}
 
 
 #endif // COMMON_MAP_H
